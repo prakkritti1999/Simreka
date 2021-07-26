1c9b07adb899fe21b22f104253a0abe25987ef3d
prakkritti1999 <67969196+prakkritti1999@users.noreply.github.com>
2021-07-26 16:02:30 +0530

    Update disapprove.php

--- disapprove.php
+++ disapprove.php
@@ -4,8 +4,19 @@
     $email=$_GET['email'];
     
     //if the admin has disapproved the user,then no=0;
-    $no=0;
-    $disapprove_query="insert into users_permission values('$email','$no')";
-    $disapprove=  mysqli_query($connect, $disapprove_query);
-    echo '<script>alert("User Disapproved"); window.location="AdminLoginView.php"</script>';
+    $no='no';
+    
+    $approvalchk_query=  mysqli_query($connect, "select * from users_permission where email='$email'");
+    $row=  mysqli_fetch_assoc($approvalchk_query);
+    if($row['is_permission']==$no){
+        echo '<script>alert("Dear Admin, You have already disapproved this user"); window.location="AdminLoginView.php";</script>';
+    }
+    elseif ($row['is_permission']=='yes') {
+        echo '<script>alert("Alert!! This user has already been approved.You cant dispapprove him/her."); window.location="AdminLoginView.php";</script>';
+    }
+    else{
+        $disapprove_query="insert into users_permission values('$email','$no')";
+        $disapprove=  mysqli_query($connect, $disapprove_query);
+        echo '<script>alert("User Disapproved"); window.location="AdminLoginView.php"</script>';
+    }
 ?>
